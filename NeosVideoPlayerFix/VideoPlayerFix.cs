f31fdbc4bb0252fee9ee7e4b7eae8b9aedc60c8d
Fro Zen <frozenreflex1@gmail.com>
2021-10-24 00:42:31 -0500

    Remove UnityVideoPlayer, update .NET version, change references

--- NeosVideoPlayerFix/VideoPlayerFix.cs
+++ NeosVideoPlayerFix/VideoPlayerFix.cs
@@ -9,7 +9,7 @@ namespace VideoPlayerFix
     {
         public override string Name => "VideoPlayerFix";
         public override string Author => "Fro Zen";
-        public override string Version => "1.0.0";
+        public override string Version => "1.1.0";
 
         private static bool _first_trigger = false;
 
@@ -17,9 +17,18 @@ namespace VideoPlayerFix
         {
             Harmony harmony = new Harmony("NeosVideoPlayerFixHarmony");
             harmony.PatchAll();
+            //as an absolute backup, Unity Native isn't even a thing anymore, so we don't have to worry about it loading
+            //if one wanted to limit this to Proton/Wine, this can be encapsulated with "if (Engine.Current.IsWine){}"
+            var engines = UnityNeos.PlaybackEngine.PlaybackEngines;
+            var nativePlayer = engines.Find(i => i.Name == "Unity Native");
+            if (nativePlayer != default)
+            {
+                engines.Remove(nativePlayer);
+                UniLog.Log("Removed Unity Native from valid playback engines.");
+            }
         }
         [HarmonyPatch(typeof(VideoTextureProvider))]
-        class VideoPlayerFixPatch
+        class VideoTextureProviderPatch
         {
             [HarmonyPrefix]
             [HarmonyPatch("GetPlaybackEngine", typeof(string))]
